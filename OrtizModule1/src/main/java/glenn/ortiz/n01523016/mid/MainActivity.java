9bafd7ff0544eced17b175bf5318e0beb7a81d39
GlennMOrtiz <55350381+GlennMOrtiz@users.noreply.github.com>
2023-06-19 17:24:28 -0400

    Initial Message, Empty

---
+++ OrtizModule1/src/main/java/glenn/ortiz/n01523016/mid/MainActivity.java
@@ -0,0 +1,14 @@
+package glenn.ortiz.n01523016.mid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
